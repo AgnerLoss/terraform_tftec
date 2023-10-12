a64c3a216c194fc9a0c63fbf308bb906b5f07b24
AgnerLoss <agner1984@yahoo.com.br>
2023-10-12 09:06:57 -0300

    vamos

--- provider.tf
+++ provider.tf
@@ -1,7 +1,4 @@
 provider "azurerm" {
   skip_provider_registration = true
   features {}   
-
-  subscription_id = "caa30e72-9633-45ce-bd6d-66d8300e4a6b"
-  tenant_id       = "1a5b6d50-9253-413e-b103-fdf9df5fabf1"
 }
