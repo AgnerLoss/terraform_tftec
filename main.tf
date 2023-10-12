ca0836cf2548996b51853010209c0b37c68c2723
AgnerLoss <agner1984@yahoo.com.br>
2023-10-12 09:51:53 -0300

    mani

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-provider "azurerm" {
-  skip_provider_registration = true
+provider "azurerm" {  
   features {}   
+  subscription_id = "caa30e72-9633-45ce-bd6d-66d8300e4a6b"
+  tenant_id       = "1a5b6d50-9253-413e-b103-fdf9df5fabf1" 
 }
 
 # Create Resource Group
