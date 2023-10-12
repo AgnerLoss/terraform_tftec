f592f1f8372f9bddb8469ff80202ff84b0270c50
AgnerLoss <agner1984@yahoo.com.br>
2023-10-11 21:24:51 -0300

    atualização de tenantid

--- provider.tf
+++ provider.tf
@@ -1,4 +1,5 @@
 provider "azurerm" {
   features {}
-  subscription_id = "caa30e72-9633-45ce-bd6d-66d8300e4a6b"    
+  subscription_id = "caa30e72-9633-45ce-bd6d-66d8300e4a6b"
+  tenant_id       = "1a5b6d50-9253-413e-b103-fdf9df5fabf1"    
 }
