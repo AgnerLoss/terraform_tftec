98fade3e8257f5e01eb83cf1f0a00fc3662a2e27
Agner <65578473+AgnerLoss@users.noreply.github.com>
2023-10-12 08:21:36 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
 provider "azurerm" {
-  features {}
-  subscription_id = "caa30e72-9633-45ce-bd6d-66d8300e4a6b"
-  tenant_id       = "1a5b6d50-9253-413e-b103-fdf9df5fabf1"    
+  skip_provider_registration = true
+  features {}   
 }
